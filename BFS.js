7569f11d6f6a09221342c398597133ebf16a2244
LuckyRathore911 <luckyrathoreofficial@gmail.com>
2024-08-19 23:43:11 +0530

    Breadth First Search

---
+++ BFS.js
@@ -0,0 +1,75 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let temp = this.root;
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  BFS() {
+    let currentNode = this.root;
+    let results = [];
+    let queue = [];
+    queue.push(currentNode);
+    while (queue.length) {
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+    return results;
+  }
+}
+
+let treeVar = new BST();
+treeVar.insert(12);
+treeVar.insert(78);
+treeVar.insert(56);
+treeVar.insert(34);
+treeVar.insert(21);
+treeVar.insert(23);
+treeVar.insert(45);
+
+console.log(treeVar.BFS());
